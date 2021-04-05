07ae4c61c2425a9f00f3f9c3f3543ec0b815f06b
Danilo <daniloaugusto0212@hotmail.com>
2021-04-05 09:25:00 -0300

    Alteração do COOKIE

--- painel/login.php
+++ painel/login.php
@@ -1,21 +1,20 @@
-<?php 
-    if (isset($_COOKIE['lembrar'])) {
-        $user = $_COOKIE['user'];
-        $password = $_COOKIE['password'];
-        $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.usuarios` WHERE user = ? AND password = ?");
-        $sql->execute(array($user,$password));
-        if ($sql->rowCount() == 1) {
-            $info = $sql->fetch();
-            $_SESSION['login'] = true;
-            $_SESSION['user'] = $user;
-            $_SESSION['password'] = $password;
-            $_SESSION['cargo'] = $info['cargo'];
-            $_SESSION['nome'] = $info['nome'];
-            $_SESSION['img'] =$info['img'];
-            Painel::redirect(INCLUDE_PATH_PAINEL);
-            
-        }
+<?php
+if (isset($_COOKIE['courses_remember'])) {
+    $user = $_COOKIE['user'];
+    $password = $_COOKIE['password'];
+    $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.usuarios` WHERE user = ? AND password = ?");
+    $sql->execute(array($user,$password));
+    if ($sql->rowCount() == 1) {
+        $info = $sql->fetch();
+        $_SESSION['login'] = true;
+        $_SESSION['user'] = $user;
+        $_SESSION['password'] = $password;
+        $_SESSION['cargo'] = $info['cargo'];
+        $_SESSION['nome'] = $info['nome'];
+        $_SESSION['img'] = $info['img'];
+        Painel::redirect(INCLUDE_PATH_PAINEL);
     }
+}
 
 ?>
 <!DOCTYPE html>
@@ -33,31 +32,31 @@
 
     <div class="box-login">
         <?php
-            if (isset($_POST['acao'])) {
-                $user = $_POST['user'];
-                $password = $_POST['password'];
-                $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.usuarios` WHERE user = ? AND password = ?");
-                $sql->execute(array($user,$password));
-                if ($sql->rowCount() == 1) {
-                    $info = $sql->fetch();
-                    //Logamos com sucesso.
-                    $_SESSION['login'] = true;
-                    $_SESSION['user'] = $user;
-                    $_SESSION['password'] = $password;
-                    $_SESSION['cargo'] = $info['cargo'];
-                    $_SESSION['nome'] = $info['nome'];
-                    $_SESSION['img'] =$info['img'];
-                    if (isset($_POST['lembrar'])) {
-                        setcookie('lembrar',true,time()+(60*60*24),'/');
-                        setcookie('user',$user,time()+(60*60*24),'/');
-                        setcookie('password',$password,time()+(60*60*24),'/');
-                    }
-                    Painel::redirect(INCLUDE_PATH_PAINEL);                    
-                }else{
-                    //Falhou
-                    echo '<div class="erro-box"><i class="fa fa-times"></i> Usuário ou senha incorretos!</div>';
+        if (isset($_POST['acao'])) {
+            $user = $_POST['user'];
+            $password = $_POST['password'];
+            $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.usuarios` WHERE user = ? AND password = ?");
+            $sql->execute(array($user,$password));
+            if ($sql->rowCount() == 1) {
+                $info = $sql->fetch();
+                //Logamos com sucesso.
+                $_SESSION['login'] = true;
+                $_SESSION['user'] = $user;
+                $_SESSION['password'] = $password;
+                $_SESSION['cargo'] = $info['cargo'];
+                $_SESSION['nome'] = $info['nome'];
+                $_SESSION['img'] = $info['img'];
+                if (isset($_POST['courses_remember'])) {
+                    setcookie('courses_remember', true, time() + (60 * 60 * 24), '/');
+                    setcookie('user', $user, time() + (60 * 60 * 24), '/');
+                    setcookie('password', $password, time() + (60 * 60 * 24), '/');
                 }
+                Painel::redirect(INCLUDE_PATH_PAINEL);
+            } else {
+                //Falhou
+                echo '<div class="erro-box"><i class="fa fa-times"></i> Usuário ou senha incorretos!</div>';
             }
+        }
         ?>
         <h2>Efetue o login:</h2>
         <form method="post">
@@ -69,7 +68,7 @@
 
             <div class="form-group-login right">
                 <label>Lembrar-me</label>
-                <input type="checkbox" name="lembrar"/>      
+                <input type="checkbox" name="courses_remember"/>      
             </div><!--form-group-login-->
             
         </form>
